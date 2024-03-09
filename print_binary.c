f8a26db5ad5c3a37f7477ea93379150464f632a6
Jones32X <jonespedro.Nezingu@gmail.com>
2024-03-09 08:02:57 -0800

    tset reverse

--- print_binary.c
+++ print_binary.c
@@ -1,34 +1,37 @@
 #include "main.h"
 
 /**
- * printf_bin - prints a binary number.
- * @val: arguments.
- * Return: 1.
+ * printf_binary - prints a binary number
+ * @num: number arguements
+ * @printed: the printed characters
+ * Return: printed charcaters
  */
-int printf_bin(va_list val)
-{
-	int flag = 0;
-	int cont = 0;
-	int i, a = 1, b;
-	unsigned int num = va_arg(val, unsigned int);
-	unsigned int p;
 
-	for (i = 0; i < 32; i++)
+int printf_binary(unsigned int num, int printed)
+{
+	int binary[32] = {0};
+	int i = 0;
+
+	if (num == 0)
 	{
-		p = ((a << (31 - i)) & num);
-		if (p >> (31 - i))
-			flag = 1;
-		if (flag)
-		{
-			b = p >> (31 - i);
-			_putchar(b + 48);
-			cont++;
-		}
-	}
-	if (cont == 0)
-	{
-		cont++;
 		_putchar('0');
+		printed++;
+		return (printed);
 	}
-	return (cont);
+
+	while (num > 0)
+	{
+		binary[i] = num % 2;
+		num /= 2;
+		i++;
+	}
+
+	while (i > 0)
+	{
+		i--;
+		_putchar('0' + binary[i]);
+		printed++;
+	}
+
+	return (printed);
 }
